90b1229eedd4251ba68cddfd722343fc81631c38
lfrenteriax <lfrenteriax@hotmail.com>
2024-01-13 02:11:49 -0500

    Update player.js

--- player.js
+++ player.js
@@ -276,7 +276,7 @@ function cargarStar(data){
 				li.appendChild(a);
 			}else{
 			    span.innerHTML=arr[i-1]['status'];
-				span.className="t";
+				span.classList.add("t");;
 				li.appendChild(a);
 				li.appendChild(span);
 			}

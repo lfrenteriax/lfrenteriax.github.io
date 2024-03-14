f37bc1f5931f7f20cbe808fa18ef862339073827
lfrenteriax <lfrenteriax@hotmail.com>
2024-03-14 15:36:42 -0500

    Create control.js

---
+++ control.js
@@ -0,0 +1,38 @@
+ console.log(mqtt)
+  
+  const url = 'wss://broker.emqx.io:8084/mqtt'
+/***
+    * Node.js
+    * This document explains how to use MQTT over TCP with both mqtt and mqtts protocols.
+    * EMQX's default port for mqtt connections is 1883, while for mqtts it is 8883.
+    */
+// const url = 'mqtt://broker.emqx.io:1883'
+
+// Create an MQTT client instance
+const options = {
+  // Clean session
+  clean: true,
+  connectTimeout: 4000,
+  // Authentication
+  clientId: 'emqx_test',
+  username: '',
+  password: '',
+}
+const client  = mqtt.connect(url, options)
+client.on('connect', function () {
+  console.log('Connected')
+  // Subscribe to a topic
+  client.subscribe('/', function (err) {
+    if (!err) {
+      // Publish a message to a topic
+      client.publish('test', 'Hello mqtt')
+    }
+  })
+})
+
+// Receive messages
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+
+})

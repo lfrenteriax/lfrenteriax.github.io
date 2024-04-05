3c7f6f30ed3b8eb4b94de09781bfdd304cd0c99d
lfrenteriax <lfrenteriax@hotmail.com>
2024-04-04 21:27:23 -0500

    Update player.js

--- player.js
+++ player.js
@@ -423,3 +423,14 @@ function cargarOpciones(){
 	listaCnt.innerHTML=opciones;
 	
 }
+
+var elem = document.documentElement;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}

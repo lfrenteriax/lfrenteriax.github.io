d93b7e56ff1b80fbe6a21ed0cb6129be91e8fc3d
lfrenteriax <lfrenteriax@hotmail.com>
2024-04-09 21:23:58 -0500

    Update player.js

--- player.js
+++ player.js
@@ -224,7 +224,7 @@ var parser = new DOMParser();
 var parsed;
 function loadEvents() { 
  // event.cargar(this.response);
-	this.cargar(this.response);
+	this.event.cargar(this.response);
  /* html = response;
  parser = new DOMParser();
 parsed = parser.parseFromString(html, "text/html");
